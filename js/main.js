4098af7f2421f52b1cd9fa4ba27bbbe592d0c625
chrisokbimpong <chris.bimpong@amalitech.org>
2020-10-09 12:40:25 +0000

    Post calls

--- js/main.js
+++ js/main.js
@@ -1,29 +1,62 @@
+function addItem(id, name, description, price, moreInfo) {
+    let html = '';
+    html += '<div class="item" data-id="' + id + '">';
+    html += '<div class="name">' + name + '</div>';
+    html += '<img src="assets/orange.jpg" alt="">';
+    html += '<div class="description">' + description + '</div>';
+    html += '<div class="price">' + price + '</div>';
+    html += '<button class="item-add">Add to cart</button>';
+    html += '<button class="item-remove">Remove</button>';
+    html += '<br>';
+    html += '<a class="more-info-link" href="#">More Info</a>';
+    html += '<div class="more-info">' + moreInfo + '</div>';
+    html += '</div>';
+
+    $('#container').prepend(html);
+}
+
 $(document).ready(function () {
-    $('#button-create-item').on('click', function() {
-        let name = $('#input-create-item').val();
-        $('#input-create-item').val('');
-
-        let html = '';
-        html += '<div class="item">';
-        html += '<div class="name">'+ name +'</div>';
-        html += '<img src="assets/orange.jpg" alt="">';
-        html += '<div class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere est nulla et quia qui! Excepturi rem itaque nulla voluptas ullam autem quod? Tempore voluptates voluptatum fugiat? Dolores incidunt voluptatibus provident?</div>';
-        html += '<div class="price">499</div>';
-        html += '<button class="item-add">Add to cart</button>';
-        html += '<button class="item-remove">Remove</button>';
-        html += '<br>';
-        html += '<a href="#">More Info</a>';
-        html += '<div class="more-info">Lorem ipsum dolor sit amet consectetur,</div>';
-        html += '</div>';
-
-        $('#container').prepend(html);
-    });
-    $('#container').on('click', '.more-info-link', function(event) {
+    $('#container').on('click', '.more-info-link', function (event) {
         event.preventDefault();
-        $(this).parent().find('.more-info').toggle();
+        $(this).parent().find('.more-info').fadeToggle('slow');
+        $(this).animate({ "opacity": 0.5, "margin-left": 10 }, 'fast').animate({ "opacity": 1.0, "margin-left": 0 }, 'fast');
     });
 
-    $('#container').on('click', '.item-remove', function() {
+    $('#container').on('click', '.item-remove', function () {
         $(this).parent().remove();
+    });
+
+    $.ajax('data/item.json', {
+        dataType: 'json',
+        contentType: 'application/json',
+        cache: false
+    })
+        .done(function (response) {
+            let items = response.items;
+            console.log(items);
+            items.forEach(function (item) {
+                addItem(item.id, item.name, item.description, item.price, item.moreInfo);
+            });
+        })
+        .fail(function (request, errorType, errorMessage) {
+            console.log(errorMessage);
+        })
+        .always(function () {
+
+        });
+    $('#container').on('click', '.item-add', function () {
+        let id = $(this).parent().data('id');
+        $.ajax('data/addToCart.json', {
+            type: 'post',
+            data: { id: id },
+            dataType: 'json',
+            contentType: 'application/json'
+        })
+        .done(function(response) {
+            if (response.message === 'success') {
+                let price = response.price;
+                console.log(price);
+            }
+        })
     });
 });
