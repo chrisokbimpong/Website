4ef489b2de1cc13d7aef66d7fc4caec339b30b94
chrisokbimpong <chris.bimpong@amalitech.org>
2020-10-09 10:27:55 +0000

    store setup

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
 $(document).ready(function () {
-    $('#select-menu').on('change', function () {
-        let name = $('#select-menu option:selected').text();
-        let distance = $('#select-menu option:selected').val();
-        let price = $('#select-menu option:selected').data('price');
-
-        if (distance) {
-            $('#feedback-message').text('You are signing up for a ' + name + ',which cost '
-                + price + ', to a distance of ' + distance + 'km');
-        } else {
-            $('#feedback-message').empty();
-        }
-    });
+    
 });
